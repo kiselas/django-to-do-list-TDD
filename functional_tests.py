9f36451c01aa6c24eca88df6b7343ba81aa0a3a1
Aspire-kisel <kisel.nf97@gmail.com>
2021-08-06 20:17:16 +0300

    ФТ теперь проверяет возможность ввести элемент списка

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,8 +14,31 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        self.browser.get('http://localhost:8001')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Купить перья')
+
+        # после нажатия Enter страница обновиться, ждем загрузку одну секунду
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # после загрузки должен появиться наш эллемент в таблице
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Купить перья' for row in rows)
+        )
+
         self.fail('Закончить тест!')
 
 if __name__ == '__main__':
