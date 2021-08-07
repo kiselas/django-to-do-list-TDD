dd967eed96c5ce5476b5c61092b2b1e02952a585
Aspire-kisel <kisel.nf97@gmail.com>
2021-08-07 08:46:10 +0300

    Модель для элементов списка и связанная с ней миграция

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """наличие строки в таблице списка"""
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
@@ -31,10 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # после нажатия Enter страница обновиться, ждем загрузку одну секунду
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        # после загрузки должен появиться наш эллемент в таблице
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table("1: Купить перья")
 
         #пробуем добавить второй эллемент
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -44,12 +47,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # после загрузки должен появиться наш эллемент в таблице
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Купить перья', [row.text for row in rows])
-        self.assertIn('2: Сделать мушку ', [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Купить перья")
+        self.check_for_row_in_list_table("2: Сделать мушку")
 
         self.fail('Закончить тест!')
 
