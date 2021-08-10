46eaaabbf6f0de7730af9650ed6de793aa79c2b0
Aspire-kisel <kisel.nf97@gmail.com>
2021-08-10 21:28:51 +0300

    добавили модель list, создание списка при добавлении новыъ элементов

--- lists/views.py
+++ lists/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     """home page"""
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/unique_id_test/')
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 def view_list(request):
     '''представление списка'''
     items = Item.objects.all()
     return render(request, 'list.html', {'items' : items})
+
+def new_list(request):
+    '''новый список'''
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/unique_id_test/')
