1914eef9a584fe71029855a3877ef5dd05cf0a10
Aspire-kisel <kisel.nf97@gmail.com>
2021-08-14 15:46:57 +0300

    Использован Bootstrap для улучшения макета, добавллен STATIC_ROOT и деактивирован admin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,14 +1,17 @@
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 
+
+
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
-    'тест нового посетителя'
+class NewVisitorTest(StaticLiveServerTestCase):
+    """тест нового посетителя"""
 
     def setUp(self):
 
@@ -102,4 +105,26 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Купить молоко', page_text)
 
+
+    def test_layout_and_styling(self):
+        """тест макета и стилевого оформления"""
+        # пользователь открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # он замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512,
+            delta=10
+        )
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
