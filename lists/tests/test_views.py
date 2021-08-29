73faf533e6d430bd6397dc50d662f5b7c65c14b8
Aspire-kisel <kisel.nf97@gmail.com>
2021-08-29 13:48:43 +0300

    Обеспечена валидация модели в представлении списка

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -45,7 +45,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'другой элемент 1 списка')
         self.assertNotContains(response, 'другой элемент 2 списка')
 
-
     def test_passes_correct_list_to_template(self):
         """тест: передается правильный шаблон списка"""
         other_list = List.objects.create()
@@ -90,3 +89,15 @@ class ListViewTest(TestCase):
         self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        """тест: ошибки валидации оканчиваются на странице списков"""
+        list_ = List.objects.create()
+        response = self.client.post(
+            f'/lists/{list_.id}/',
+            data={'item_text' : ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
